0d488540b33d4b6411c2f884172007b6f7d6bbdf
Osiota Samuel Obrozie <73504914+osiota10@users.noreply.github.com>
2023-06-26 18:05:43 +0100

    Update 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,12 +2,10 @@ export default function taskBlock(trueOrFalse) {
   let task = false;
   let task2 = true;
 
-  
   if (trueOrFalse) {
-    let task = true;
-    let task2 = false;
+    task = true;
+    task2 = false;
   }
-  
 
   return [task, task2];
 }
